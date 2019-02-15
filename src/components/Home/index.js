04a7095b8aceb45284fe8399eb8c9e64ce4fa5e9
ai-cuicui <ai_hasana@163.com>
2019-02-15 17:00:58 +0800

    提交

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,25 @@
 import React, { PureComponent } from 'react';
+import './index.scss'
 
 class Home extends PureComponent {
     render () { 
         return (
-            <div>
-                Home
+            <div className="home">
+                <div className="main">
+                    <div>
+                        <p>在线统计</p>
+                        
+                    </div>
+                    <div>
+                        <p>故障统计</p>
+                    </div>
+                    <div>
+                        <p>网关型号统计</p>
+                    </div>
+                    <div>
+                        <p>故障类型统计</p>
+                    </div>
+                </div>
             </div>
         );
     }
